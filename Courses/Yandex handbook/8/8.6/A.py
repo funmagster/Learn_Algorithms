c8084a8954c62210c9d9111e3ce89e3a33ab6388
kulibabast <kulibabast@gmail.com>
2024-05-16 14:50:44 +0300

    Solve 8.6

--- Courses/Yandex handbook/8/8.6/A.py
+++ Courses/Yandex handbook/8/8.6/A.py
@@ -1,8 +1,18 @@
-def knapsack(w, n, weight):
-    "I'll decide later"
-    return 1
+def knapsack(w, n, weight, pack):
+    if (w, n) not in pack:
+        if n == 0 or w == 0:
+            pack[(w, n)] = 0
+        elif weight[n - 1] > w:
+            pack[(w, n)] = knapsack(w, n-1, weight, pack)
+        else:
+            pack[(w, n)] = max(
+                weight[n - 1] + knapsack(w - weight[n - 1], n-1, weight, pack),
+                knapsack(w, n-1, weight, pack)
+            )
+    return pack[(w, n)]
 
 
 w, n = map(int, input().split())
 weight = list(map(int, input().split()))
-print(knapsack(w, n, weight))
+pack = {}
+print(knapsack(w, n, weight, pack))
